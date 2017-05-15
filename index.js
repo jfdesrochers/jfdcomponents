83f1d37f548907a48ff4c24d463e258c98066823
Jean-Francois Desrochers <jfdesrochers@gmail.com>
2017-05-15 13:44:11 -0400

    Added Validating Controls

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ module.exports.Property = require('./components/property.js')
 module.exports.StateManager = require('./components/statemanager.js')
 module.exports.Animator = require('./components/animator.js')
 module.exports.Translator = require('./components/translator.js')
+
+module.exports.ValidationManager = require('./components/ui/forms.js').ValidationManager
+module.exports.ValidatingInput = require('./components/ui/forms.js').ValidatingInput
+module.exports.ValidatingSelect = require('./components/ui/forms.js').ValidatingSelect
